e59174d37a29b11f15c3b68e3c4a17018a58a89d
Gerard Montilla Romia <gerardmr90@gmail.com>
2017-05-24 16:37:35 +0200

    Server sending notifications to receiver when a parcel is delivered

--- server/functions/index.js
+++ server/functions/index.js
@@ -9,23 +9,63 @@ admin.initializeApp(functions.config().firebase);
 exports.newDelivery = functions.database.ref('/receiver_deliveries/{receiver_uid}/{delivery_uid}').onWrite(event => {
     const snapshot = event.data;
 
-    if (!snapshot.exists()) {
+    if (snapshot.child('state').val() === "Delivered") {
         return;
     }
 
-    // notificationData = {};
-    // notificationData["commerce_name"] = snapshot.child("commerce_name").val();
-    // notificationData["commerce_uid"] = snapshot.child("commerce_uid").val();
-    // notificationData["removed"] = snapshot.child("removed").val().toString();
-    // notificationData["title"] = snapshot.child("title").val();
-    // notificationData["description"] = snapshot.child("description").val();
-    // notificationData["used"] = snapshot.child("used").val().toString();
-    // notificationData["description"] = snapshot.child("description").val();
+    if (!snapshot.exists()) {
+        return;
+    }
 
     const payload = {
         notification: {
             title: 'Delivery created',
             body: 'A delivery has been created'
+        }
+    };
+
+    const getToken = admin.database().ref(`/receivers/${event.params.receiver_uid}/token`).once('value');
+
+    return Promise.all([getToken]).then(results => {
+        const token = results[0];
+        if(token.val()){
+            const tokens = token.val();
+            //Send notifications to all tokens.
+            return admin.messaging().sendToDevice(tokens, payload).then(response =>{
+                //For each message check if there was an error.
+                const tokensToRemove = [];
+                response.results.forEach((result, index) => {
+                    const error = result.error;
+                    if(error){
+                        console.error('Failure sending notification to', tokens[index],error);
+                        //Cleanup the tokens who are not registered anymore.
+                        if (error.code === 'messaging/invalid-registration-token' ||
+                        error.code === 'messaging/registration-token-not-registered') {
+                            tokensToRemove.push(allTokens.ref.child(tokens[index]).remove());
+                        }
+                    }
+                });
+                return Promise.all(tokensToRemove);
+            });
+        }
+    });
+});
+
+exports.parcelDelivered = functions.database.ref('/receiver_deliveries/{receiver_uid}/{delivery_uid}/').onWrite(event => {
+    const snapshot = event.data;
+
+    if (!snapshot.exists()) {
+        return;
+    }
+
+    if (snapshot.child('state').val() === "Created") {
+        return;
+    }
+
+    const payload = {
+        notification: {
+            title: 'Parcel delivered',
+            body: 'Your parcel has been succesfully delivered'
         }
     };
 
